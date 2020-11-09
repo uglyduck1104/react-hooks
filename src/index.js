3f9c376b1dd1394b444a923cae65a73d215e2199
YongJun Lee <60164186+uglyduck1104@users.noreply.github.com>
2020-11-09 22:56:08 +0900

    #useInput #1.1 useInput #1.2 useInput part Two

--- src/index.js
+++ src/index.js
@@ -2,50 +2,33 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 
+const useInput = (initalValue, validator) => {
+  const [value, setValue] = useState(initalValue);
+  const onChange = (event) => {
+    const {
+      target: { value }
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
+
 const App = () => {
-  const [item, setItem] = useState(1);
-  const incrementItem = () => setItem(item + 1);
-  const decrementItem = () => setItem(item - 1);
+  const maxLen = (value) => !value.includes("@");
+  const name = useInput("Mr.", maxLen);
   return (
     <div className="App">
-      <h1>Hello {item}</h1>
-      <h2>Start editing to see some magin happen!</h2>
-      <button onClick={incrementItem}>incrementItem</button>
-      <button onClick={decrementItem}>decrementItem</button>
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
     </div>
   );
 };
 
-class AppUgly extends React.Component {
-  state = {
-    item: 1
-  };
-  render() {
-    const { item } = this.state;
-    return (
-      <div className="App">
-        <h1>Hello {item}</h1>
-        <h2>Start editing to see some magin happen!</h2>
-        <button onClick={this.incrementItem}>incrementItem</button>
-        <button onClick={this.decrementItem}>decrementItem</button>
-      </div>
-    );
-  }
-  incrementItem = () => {
-    this.setState((state) => {
-      return {
-        item: state.item + 1
-      };
-    });
-  };
-  decrementItem = () => {
-    this.setState((state) => {
-      return {
-        item: state.item - 1
-      };
-    });
-  };
-}
-
 const rootElement = document.getElementById("root");
-ReactDOM.render(<AppUgly />, rootElement);
+ReactDOM.render(<App />, rootElement);
